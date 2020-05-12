cc5dd06cd0769ee70e09d7014f0b7ba85691722b
Ibrahim Estanga <ibrahim.estanga@rappi.com>
2020-05-12 12:52:09 -0500

    init commit

---
+++ app/src/main/java/com/ibrahim/dev/localbitcoinutils/app/Application.kt
@@ -0,0 +1,10 @@
+package com.ibrahim.dev.localbitcoinutils.app
+
+import android.app.Application
+
+class Application: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
